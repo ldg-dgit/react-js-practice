cad547ff4da9942377bf0a37c736a51d7ba370f8
Bobby Lee <ldg@dgit.co>
2022-08-17 15:04:21 +0900

    create react app

--- react-js-app-practice/src/App.js
+++ react-js-app-practice/src/App.js
@@ -1,7 +1,11 @@
+import Button from "./Button.js";
+import styles from "./App.module.css";
+
 function App() {
   return (
-    <div>
+    <div className={styles.title}>
       <h1>Hi!!</h1>
+      <Button text={"Continue"} />
     </div>
   );
 }
