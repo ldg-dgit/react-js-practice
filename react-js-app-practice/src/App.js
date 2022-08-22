d010c3063fd78fc9eb9bafd82476c120617a87a9
LDG - DGit <hi-ldg@dgit.co>
2022-08-23 02:15:18 +0900

    made counter use state

--- react-js-app-practice/src/App.js
+++ react-js-app-practice/src/App.js
@@ -1,11 +1,14 @@
 import Button from "./Button.js";
 import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
   return (
-    <div className={styles.title}>
-      <h1>Hi!!</h1>
-      <Button text={"Continue"} />
+    <div>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click Me</button>
     </div>
   );
 }
