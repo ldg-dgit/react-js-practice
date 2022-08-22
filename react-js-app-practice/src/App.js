ecd1fa1e6a8554f152c39a3579de4cfefc7bba54
LDG - DGit <hi-ldg@dgit.co>
2022-08-23 03:16:57 +0900

    counter with react effect

--- react-js-app-practice/src/App.js
+++ react-js-app-practice/src/App.js
@@ -1,12 +1,28 @@
 import Button from "./Button.js";
 import styles from "./App.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  useEffect(() => {
+    console.log("CALL THE API....");
+    console.log("I run only once.");
+  }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when keyword & counter change");
+  }, [keyword, counter]);
   return (
     <div>
+      <input value={keyword} onChange={onChange} type='text' placeholder='Search here...' />
       <h1>{counter}</h1>
       <button onClick={onClick}>Click Me</button>
     </div>
