69749cf3cc99752562a4ad3e6ae9c8821c7db9d4
LDG - DGit <hi-ldg@dgit.co>
2022-08-23 22:46:07 +0900

    Cryptocurrency Converter App

--- react-js-app-practice/src/App.js
+++ react-js-app-practice/src/App.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [myMoney, setMyMoney] = useState([]);
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
@@ -11,17 +12,31 @@ function App() {
         setLoading(false);
       });
   }, []);
+  function onChange(event) {
+    setMyMoney(event.target.value);
+  }
   return (
     <div>
-      <h1>Cryptocurrency Price ({coins.length})</h1>
+      <h1>Cryptocurrency Converter ({coins.length})</h1>
       {loading ? <strong>Loading...</strong> : null}
-      <ul>
-        {coins.map((coin) => (
-          <li>
-            {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
-          </li>
-        ))}
-      </ul>
+      <input
+        onChange={onChange}
+        value={myMoney}
+        type='number'
+        placeholder='Please Write your USD'
+      />
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select>
+          {coins.map((coin) => (
+            <option>
+              {coin.name} ({coin.symbol}):
+              {myMoney / coin.quotes.USD.price} {coin.symbol}
+            </option>
+          ))}
+        </select>
+      )}
     </div>
   );
 }
