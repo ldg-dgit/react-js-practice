6608da0f4d8eee4df482680fc359c2f349a05a06
LDG - DGit <hi-ldg@dgit.co>
2022-08-24 14:35:18 +0900

    add github pages & bug fixed

--- react-js-app-practice/src/App.js
+++ react-js-app-practice/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />}></Route>
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />}></Route>
         <Route path='/movie/:id' element={<Detail />}></Route>
       </Routes>
     </BrowserRouter>
