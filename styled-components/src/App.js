2406d01e1d11466a4641f953b7b4d89f5cbe0804
LDG - DGit <hi-ldg@dgit.co>
2022-11-21 20:19:41 +0900

    Adapting and Extending

--- styled-components/src/App.js
+++ styled-components/src/App.js
@@ -4,29 +4,21 @@ const Father = styled.div`
   display: flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: tomato;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
 
-const BoxTwo = styled.div`
-  background-color: teal;
-  width: 100px;
-  height: 100px;
-`;
-
-const Text = styled.span`
-  color: white;
+const Circle = styled(Box)`
+  border-radius: 50px;
 `;
 
 function App() {
   return (
     <Father>
-      <BoxOne>
-        <Text>Hello World</Text>
-      </BoxOne>
-      <BoxTwo />
+      <Box bgColor='teal' />
+      <Circle bgColor='tomato' />
     </Father>
   );
 }
