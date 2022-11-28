6df02661c931f2caba97f9a7ba4bb82d631fe67d
LDG - DGit <hi-ldg@dgit.co>
2022-11-28 15:24:17 +0900

    As and Attrs

--- styled-components/src/App.js
+++ styled-components/src/App.js
@@ -4,21 +4,17 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
-
-const Circle = styled(Box)`
-  border-radius: 50px;
+const Input = styled.input.attrs({ required: true })`
+  background-color: tomato;
 `;
 
 function App() {
   return (
     <Father>
-      <Box bgColor='teal' />
-      <Circle bgColor='tomato' />
+      <Input />
+      <Input />
+      <Input />
+      <Input as='button' />
     </Father>
   );
 }
