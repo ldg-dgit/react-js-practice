3977cd8609196f29d57ba4a7ef28bc91545c92a2
LDG - DGit <hi-ldg@dgit.co>
2022-12-02 02:33:11 +0900

    create typescript-practice react app

---
+++ typescript-practice/src/App.tsx
@@ -0,0 +1,24 @@
+import styled, { keyframes } from "styled-components";
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => props.theme.backgroundColor};
+`;
+
+function App() {
+  return (
+    <Wrapper>
+      <Title>Hello, World</Title>
+    </Wrapper>
+  );
+}
+
+export default App;
