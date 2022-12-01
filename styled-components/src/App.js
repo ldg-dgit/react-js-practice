875e8c6c266048569130039cc89852312fc07b30
LDG - DGit <hi-ldg@dgit.co>
2022-12-02 02:13:01 +0900

    theme

--- styled-components/src/App.js
+++ styled-components/src/App.js
@@ -1,7 +1,7 @@
 import styled, { keyframes } from "styled-components";
 
 const Title = styled.h1`
-  color: green;
+  color: ${(props) => props.theme.textColor};
 `;
 
 const Wrapper = styled.div`
@@ -10,56 +10,13 @@ const Wrapper = styled.div`
   height: 100vh;
   justify-content: center;
   align-items: center;
-  ${Title}:hover {
-    color: red;
-  }
-`;
-
-const Box = styled.div`
-  height: 100px;
-  width: 100px;
-  background-color: ${(props) => props.bgColor};
-  display: flex;
-`;
-
-const Circle = styled(Box)`
-  border-radius: 50px;
-`;
-
-const Btn = styled.button`
-  color: yellow;
-`;
-
-const Input = styled.input.attrs({ required: true, maxLength: 20 })`
-  background-color: blue;
-`;
-
-const ani = keyframes`
-from{
-  color: tomato
-} to {
-color:teal
-}
-
-`;
-
-const AniBtn = styled.button`
-  animation: ${ani} 0.5s infinite;
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box bgColor='tomato' />
-      <Circle bgColor='teal' />
-      <Btn>Log in</Btn>
-      <Btn as='a'>Link</Btn>
-      <Input />
-      <Input />
-      <Input />
-      <AniBtn>Hihi</AniBtn>
       <Title>Hello, World</Title>
-      <h1>Hi hi</h1>
     </Wrapper>
   );
 }
--- styled-components/src/index.js
+++ styled-components/src/index.js
@@ -1,11 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+const darkTheme = {
+  textColor: "whitesmoke",
+  backgroundColor: "#111",
+};
+
+const lightTheme = {
+  textColor: "#111",
+  backgroundColor: "whitesmoke",
+};
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
