4ed5f0cde8323d44f22d010a7056b451f878427f
LDG - DGit <hi-ldg@dgit.co>
2022-12-02 11:33:56 +0900

    Forms

--- typescript-practice/src/App.tsx
+++ typescript-practice/src/App.tsx
@@ -1,11 +1,24 @@
+import React, { useState } from "react";
 import styled from "styled-components";
-import Circle from "./Circle";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", value);
+  };
   return (
     <div>
-      <Circle bgColor='teal' borderColor='black' />
-      <Circle bgColor='tomato' text="i'm tomato" />
+      <form onSubmit={onSubmit}>
+        <input value={value} onChange={onChange} type='text' placeholder='username' />
+        <button>Sign In</button>
+      </form>
     </div>
   );
 }
