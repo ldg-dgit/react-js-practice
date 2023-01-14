cad91ecff8e47b48d2d9f7388146894dab30621e
Bobby Lee <ldg@dgit.co>
2023-01-14 15:27:59 +0900

    rotate box

--- framer-motion-practice/src/App.tsx
+++ framer-motion-practice/src/App.tsx
@@ -13,14 +13,18 @@ const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: white;
-  border-radius: 10px;
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box transition={{ duration: 2 }} animate={{ borderRadius: "100px" }} />
+      <Box
+        transition={{ type: "spring", bounce: 0.6, delay: 0.5 }}
+        initial={{ scale: 0 }}
+        animate={{ scale: 1, rotateZ: 360 }}
+      />
     </Wrapper>
   );
 }
