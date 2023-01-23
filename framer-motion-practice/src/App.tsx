8c1c654109c6b1b9840c56faba4a8d2250771b09
Bobby Lee <ldg@dgit.co>
2023-01-23 20:42:44 +0900

    add drag limit box

--- framer-motion-practice/src/App.tsx
+++ framer-motion-practice/src/App.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { useRef } from "react";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -7,6 +8,17 @@ const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+`;
+
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.4);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
 `;
 
 const Box = styled(motion.div)`
@@ -19,15 +31,25 @@ const Box = styled(motion.div)`
 `;
 
 const boxVariants = {
-  hover: { scale: 1.5, rotateZ: 90 },
+  hover: { scale: 1.2, rotateZ: 90 },
   click: { scale: 1, borderRadius: "100px" },
-  drag: { backgroundColor: "rgb(252, 67, 39)", transition: { duration: 1 } },
 };
 
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
   return (
     <Wrapper>
-      <Box drag variants={boxVariants} whileHover='hover' whileDrag='drag' whileTap='click' />
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag
+          dragSnapToOrigin
+          dragElastic={0.25}
+          dragConstraints={biggerBoxRef}
+          variants={boxVariants}
+          whileHover='hover'
+          whileTap='click'
+        />
+      </BiggerBox>
     </Wrapper>
   );
 }
