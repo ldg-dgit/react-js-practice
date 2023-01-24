e53d123e1c781a3f0463b098b4a0036f6bf7347c
Bobby Lee <ldg@dgit.co>
2023-01-24 13:53:42 +0900

    box slider animation

--- framer-motion-practice/src/App.tsx
+++ framer-motion-practice/src/App.tsx
@@ -13,48 +13,75 @@ const Wrapper = styled(motion.div)`
 `;
 
 const Box = styled(motion.div)`
-  width: 175px;
-  height: 175px;
-  display: grid;
+  width: 100px;
+  height: 100px;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 25px;
   background-color: rgba(255, 255, 255, 1);
-  border-radius: 40px;
+  border-radius: 25px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxVariants = {
-  initial: {
-    opacity: 0,
-    scale: 0,
+const box = {
+  entry: (isBack: boolean) => {
+    return {
+      x: isBack ? -500 : 500,
+      opacity: 0,
+      scale: 0,
+    };
   },
-  visible: {
+  center: {
+    x: 0,
     opacity: 1,
     scale: 1,
-    rotateZ: 360,
+    transition: {
+      duration: 1,
+    },
   },
-  leaving: {
-    opacity: 0,
-    scale: 0,
-    y: 20,
+  exit: (isBack: boolean) => {
+    return {
+      x: isBack ? 500 : -500,
+      opacity: 0,
+      scale: 0,
+    };
   },
 };
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const toggleShowing = () => setShowing((prev) => !prev);
+  const [visible, setVisible] = useState(1);
+  const [back, setBack] = useState(false);
+  const nextPlease = () => {
+    setBack(false);
+    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+  };
+  const prevPlease = () => {
+    setBack(true);
+    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+  };
   return (
     <Wrapper>
-      <AnimatePresence>
-        {showing ? (
-          <Box
-            style={{ marginBottom: "50px" }}
-            variants={boxVariants}
-            initial='initial'
-            animate='visible'
-            exit='leaving'
-          />
-        ) : null}
+      <AnimatePresence custom={back}>
+        <Box
+          custom={back}
+          key={visible}
+          variants={box}
+          initial='entry'
+          animate='center'
+          exit='exit'
+          transition={{ duration: "1" }}
+        >
+          {visible}
+        </Box>
       </AnimatePresence>
-      <button onClick={toggleShowing}>click</button>
+      <button onClick={prevPlease} style={{ position: "absolute", bottom: "100px" }}>
+        prev
+      </button>
+      <button onClick={nextPlease} style={{ position: "absolute", bottom: "120px" }}>
+        next
+      </button>
     </Wrapper>
   );
 }
